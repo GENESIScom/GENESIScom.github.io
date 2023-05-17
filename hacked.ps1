98238607741679f736f6d7a07d8f6b741f0f7c91
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2023-05-17 14:23:40 +0200

    Update hacked.ps1

--- hacked.ps1
+++ hacked.ps1
@@ -1,4 +1,4 @@
 while (1){
 Add-Type -AssemblyName PresentationFramework 
-[System.Windows.MessageBox]::Show('Get hacked Kid', 'DefaultDesktopOnly')
+[System.Windows.MessageBox]::Show('Get hacked Kid', 'EZZZ retard', '1', '2', '3', 'DefaultDesktopOnly')
 }
