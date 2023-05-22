acc36c92730b742a9293077147a4f05dcadc5670
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2023-05-22 10:10:47 +0200

    Create runAll.ps1

---
+++ runAll.ps1
@@ -0,0 +1,3 @@
+IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/mouseMove.ps1')
+IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/minimizeAll.ps1')
+IEX (New-Object Net.WebClient).DownloadString('https://creepyturnip.github.io/msgboxLoop.ps1')
